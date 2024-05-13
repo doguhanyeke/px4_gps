8b2383c7d4352d67ec0eacd91a3a065f3b6b08f0
Kartik Anand Pant <kpant14@gmail.com>
2024-05-12 21:04:50 -0400

    Add time based attack

---
+++ launch/px4_gps_xrce_attack.launch.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+############################################################################
+#
+#   Copyright (C) 2022 PX4 Development Team. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in
+#    the documentation and/or other materials provided with the
+#    distribution.
+# 3. Neither the name PX4 nor the names of its contributors may be
+#    used to endorse or promote products derived from this software
+#    without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+# OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+# AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+############################################################################
+
+__author__ = "Kartik Anand Pant"
+__contact__ = "kpant14@gmail.com"
+
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    px4_ns = LaunchConfiguration('px4_ns')
+    gz_world_name = LaunchConfiguration('gz_world_name')
+    gz_model_name = LaunchConfiguration('gz_model_name')
+    gz_spoofer_model_name = LaunchConfiguration('gz_spoofer_model_name')
+    gps_delay = LaunchConfiguration('gps_delay')
+
+    px4_ns_arg = DeclareLaunchArgument(
+        'px4_ns',
+        default_value='px4_1'
+    )
+    gz_world_name_arg = DeclareLaunchArgument(
+        'gz_world_name',
+        default_value='AbuDhabi'
+    )
+    gz_model_name_arg = DeclareLaunchArgument(
+        'gz_model_name',
+        default_value='x500_1'
+    )
+    gz_spoofer_model_name_arg = DeclareLaunchArgument(
+        'gz_spoofer_model_name',
+        default_value='spoofer'
+    )
+    gps_delay_arg = DeclareLaunchArgument(
+        'gps_delay',
+        default_value='10'
+    )
+
+    falsi_config = os.path.join(
+        get_package_share_directory('px4_gps'),
+        'config',
+        'falsi.yaml'
+    )
+    mavros_gps_node = Node(
+           package='px4_gps',
+           executable='px4_gps_pub_xrce',
+           name='px4_gps_pub_xrce',
+           parameters=[
+                {'px4_ns': px4_ns},
+                {'gz_world_name': gz_world_name},
+                {'gz_model_name': gz_model_name},
+                {'gz_spoofer_model_name': gz_spoofer_model_name},
+                {'gps_delay': gps_delay},
+                falsi_config
+            ]
+    )
+
+    return LaunchDescription([
+        px4_ns_arg,
+        gz_world_name_arg,
+        gz_model_name_arg,
+        gz_spoofer_model_name_arg,
+        gps_delay_arg,
+        mavros_gps_node,
+    ])
--- src/px4_gps_pub_xrce.cpp
+++ src/px4_gps_pub_xrce.cpp
@@ -16,10 +16,21 @@
 #include <geometry_msgs/msg/pose.hpp>
 #include <std_msgs/msg/bool.hpp>
 #include "offboard_detector/msg/detector_output.hpp"
+#include <vector>
+#include <math.h>
+#include <cmath>
 using namespace std::chrono_literals;
 
 /* This example creates a subclass of Node and uses std::bind() to register a
 * member function as a callback from the timer. */
+class Attack {
+public:
+    double time_t;
+    double x;
+    double y;
+    double z;
+    int victim_robot_id;
+};
 
 class PX4GPSXrcePublisher : public rclcpp::Node
 {
@@ -31,11 +42,35 @@ class PX4GPSXrcePublisher : public rclcpp::Node
       this->declare_parameter("gz_world_name", "AbuDhabi");
       this->declare_parameter("gz_model_name", "x500_1");
       this->declare_parameter("gz_spoofer_model_name", "spoofer");
+      //this->declare_parameter("attack_parameters", std::vector<double>{});
 
       std::string _px4_ns = this->get_parameter("px4_ns").as_string();
       std::string _world_name = this->get_parameter("gz_world_name").as_string();
       std::string _model_name = this->get_parameter("gz_model_name").as_string();
       std::string _spoofer_model_name = this->get_parameter("gz_spoofer_model_name").as_string();
+      this->declare_parameter<std::vector<double>>("attack_parameters",std::vector<double>{} );
+      std::vector<double> attack_parameters;
+      this->get_parameter("attack_parameters", attack_parameters);
+      //std::vector<double> attack_parameters = this->get_parameter("attack_parameters").as_double_array();
+      RCLCPP_INFO(this->get_logger(),"Size: %ld", attack_parameters.size());
+      
+
+      for (size_t i = 0; i < attack_parameters.size(); i += 5)
+      {
+          Attack attack;
+          attack.time_t = attack_parameters[i];
+          attack.x = attack_parameters[i + 1];
+          attack.y = attack_parameters[i + 2];
+          attack.z = attack_parameters[i + 3];
+          attack.victim_robot_id = static_cast<int>(attack_parameters[i + 4]);
+          RCLCPP_INFO(this->get_logger(), "%d %d",attack.victim_robot_id,  _px4_ns.back()-'0');
+
+          if (attack.victim_robot_id == _px4_ns.back()-'0')
+          {
+            _attack_sequence.push_back(attack);
+            RCLCPP_INFO(this->get_logger(), "%f %f %f %f %d", attack.time_t, attack.x, attack.y, attack.z, attack.victim_robot_id);
+          }    
+      }
 
       RCLCPP_INFO(this->get_logger(),"PX4 Namespace: %s", _px4_ns.c_str());
       RCLCPP_INFO(this->get_logger(),"World Name: %s", _world_name.c_str());
@@ -46,11 +81,13 @@ class PX4GPSXrcePublisher : public rclcpp::Node
       std::string spoofer_topic = "/world/" + _world_name + "/model/" + _spoofer_model_name + "/link/base_link/sensor/navsat_sensor/navsat_multipath";
       
       std::string px4_gps_ros_topic= "";
+      std::string px4_all_hover_topic= "";
       if (_px4_ns.empty()){
         px4_gps_ros_topic = "/fmu/in/vehicle_gps_position";
       }
       else{
         px4_gps_ros_topic = "/" +  _px4_ns + "/fmu/in/vehicle_gps_position";
+        px4_all_hover_topic = "/" +  _px4_ns + "/fmu/out/all_hover";
       }
       RCLCPP_INFO(this->get_logger(),"PX4 GPS Topic: %s", px4_gps_ros_topic.c_str());
 
@@ -60,6 +97,7 @@ class PX4GPSXrcePublisher : public rclcpp::Node
       if (!_node.Subscribe(spoofer_topic, &PX4GPSXrcePublisher::navsatSpooferCallback, this)) {
             RCLCPP_ERROR(this->get_logger(),"failed to subscribe to %s", spoofer_topic.c_str());
       }  
+      
       rmw_qos_profile_t qos_profile = rmw_qos_profile_default;
       qos_profile.history=RMW_QOS_POLICY_HISTORY_KEEP_LAST;
       qos_profile.depth=1;
@@ -72,11 +110,19 @@ class PX4GPSXrcePublisher : public rclcpp::Node
         "/attack_flag", 10, std::bind(&PX4GPSXrcePublisher::attackFlagCallback, this, std::placeholders::_1));      
       detector_flag_subscriber_ = this->create_subscription<offboard_detector::msg::DetectorOutput>(
         "/px4_1/detector/out/observer_output", qos_, std::bind(&PX4GPSXrcePublisher::detectorFlagCallback, this, std::placeholders::_1));
+      all_hover_subscriber_ = this->create_subscription<std_msgs::msg::Bool>(
+        px4_all_hover_topic , qos_, std::bind(&PX4GPSXrcePublisher::allHoverCallback, this, std::placeholders::_1)); 
+
       count=0;
+      hover_flag = 0;
+      time_offset = 0.0;
+      attack_idx = 0;
+      attack_counter = 10;
     }
   private:
     void navsatCallback(const gz::msgs::NavSatMultipath &navsat)
     {   
+        
         const uint64_t time_us = (navsat.header().stamp().sec() * 1000000) + (navsat.header().stamp().nsec() / 1000);
         // publish gps
         auto sensor_gps = px4_msgs::msg::SensorGps();
@@ -110,6 +156,35 @@ class PX4GPSXrcePublisher : public rclcpp::Node
           sensor_gps.latitude_deg = spoofer_latitude;
           sensor_gps.longitude_deg = spoofer_longitude;
         }
+
+        if (all_hover==1 && hover_flag ==0)
+        {
+          start_time = time_us;
+          hover_flag = 1;
+        }
+        if (hover_flag == 1)
+        {
+          time_offset = std::round(((time_us - start_time)*1e-6)/0.001)*0.001;  
+          //RCLCPP_INFO(this->get_logger(),"Time Offset: %f", time_offset);          
+         
+          if (attack_idx < _attack_sequence.size() && time_offset > _attack_sequence[attack_idx].time_t)
+          {
+            RCLCPP_INFO(this->get_logger(),"Time Offset: %f \t %f", time_offset ,_attack_sequence[attack_idx].time_t );          
+            RCLCPP_INFO(this->get_logger(),"Old LatLonAlt: %f \t %f \t %f", sensor_gps.latitude_deg , sensor_gps.longitude_deg, sensor_gps.altitude_msl_m);  
+            sensor_gps.latitude_deg  = navsat.latitude_deg()  + (_attack_sequence[attack_idx].y / 63710000) * (180 / M_PI);
+            sensor_gps.longitude_deg =  navsat.longitude_deg() + (_attack_sequence[attack_idx].x / 63710000) * (180 / M_PI) / cos(navsat.latitude_deg() * M_PI/180);
+            sensor_gps.altitude_msl_m = navsat.altitude() + _attack_sequence[attack_idx].z;
+            
+            RCLCPP_INFO(this->get_logger(),"Attack LatLonAlt: %f \t %f \t %f", sensor_gps.latitude_deg , sensor_gps.longitude_deg, sensor_gps.altitude_msl_m);  
+
+            attack_counter--;
+            if (attack_counter == 0 )
+            {
+              attack_counter = 10;
+              attack_idx ++;
+            }
+          }
+        }
         sensor_gps.timestamp_time_relative = 0;
         sensor_gps.heading = NAN;
         sensor_gps.heading_offset = NAN;
@@ -128,6 +203,11 @@ class PX4GPSXrcePublisher : public rclcpp::Node
       attack_flag = msg->data;
     }
     
+    void allHoverCallback(const std_msgs::msg::Bool::SharedPtr msg)
+    {
+      all_hover = msg->data;
+    }
+
     void detectorFlagCallback(const offboard_detector::msg::DetectorOutput::SharedPtr msg)
     {
       detector_flag = msg->attack_detect;
@@ -145,6 +225,10 @@ class PX4GPSXrcePublisher : public rclcpp::Node
     rclcpp::Publisher<px4_msgs::msg::SensorGps>::SharedPtr gps_publisher_;
     rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr attack_flag_subscriber_;
     rclcpp::Subscription<offboard_detector::msg::DetectorOutput>::SharedPtr detector_flag_subscriber_;
+    
+    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr all_hover_subscriber_;
+
+
     gz::transport::Node _node;
     int count;
     double spoofer_latitude;
@@ -153,6 +237,13 @@ class PX4GPSXrcePublisher : public rclcpp::Node
     double spoofer_velocity_north;
     int attack_flag;
     int detector_flag;
+    int all_hover;
+    int hover_flag;
+    uint64_t start_time;
+    double time_offset;
+    std::vector<Attack> _attack_sequence;
+    size_t attack_idx;
+    int attack_counter;
     
 };
 
